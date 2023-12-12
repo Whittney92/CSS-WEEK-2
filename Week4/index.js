5ff88db97da45186a77b703dabb6fe6632455582
Whittney92 <rudolphw880@gmail.com>
2023-12-12 10:56:52 -0500

    run week 9

---
+++ Week4/index.js
@@ -0,0 +1,26 @@
+alert("open class intro");
+
+
+
+/*
+Question 2: Addition Operation
+
+Question 2: Addition Operation
+
+You have been given two variables, num1 and num2, 
+each with predefined values as test cases.
+ Your task is to calculate the sum of these variables 
+ and display the result using the console.log()
+*/
+
+
+var num1 = 10;
+var num2 = 2;
+
+/*add the variable and display using console.log*/
+console.log(num1 + num2);
+
+
+var * = 45;
+var y = 16;
+x = x - y;
